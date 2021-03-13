9f390773c3087b87d1802a25d15797179a63df1d
Dangdat <raymonddangdat@gmail.com>
2021-03-13 09:31:33 +0100

    Completed the Shimmer stuff

--- lib/widgets/food_list_view.dart
+++ lib/widgets/food_list_view.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:shimmer_loading_effect/data.dart';
 
 class FoodListView extends StatelessWidget {
   final List<Food> foodItems;
+  final bool isLoading;
 
-  const FoodListView({required this.foodItems});
+  const FoodListView({required this.foodItems, this.isLoading = false});
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
+    return isLoading ? SliverList(
       delegate: SliverChildBuilderDelegate(
         (context, index) {
+          return ListTile(
+            leading: Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(height: 60.0, width: 60.0, color: Colors.grey[300],)),
+            title: Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(height: 16.0, color: Colors.grey[300],)),
+            subtitle: Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(height: 16.0, color: Colors.grey[300],)),
+            trailing: Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(height: 24.0, width: 24.0, color: Colors.grey[300],)),
+          );
+        },
+        childCount: 10,
+      ),
+    ) : SliverList(
+      delegate: SliverChildBuilderDelegate(
+            (context, index) {
           final food = foodItems[index];
           return ListTile(
             leading: Image.network(
